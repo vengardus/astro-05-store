145698836c22959420754262b4cdef6eb4b3d608
Ismytv <ismytv@gmail.com>
2024-10-09 21:34:07 -0500

    feat(db):crea tablas User, Role y el seed

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,27 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const User = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    name: column.text(),
+    email: column.text({ unique: true }),
+    password: column.text(),
+    createdAt: column.date({ default: new Date() }),
+    role: column.text({references: () => Role.columns.id}) // user, admin, superadmin
+  }
+})
+
+const Role = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    name: column.text()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    User,
+    Role
+  }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,33 @@
-import { db } from 'astro:db';
+import { db, Role, User } from 'astro:db';
+import { v4 as uuid } from 'uuid'
+import  bcrypt from 'bcryptjs'
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+
+	const roles = [
+		{ id: 'admin', name: 'Administrador' },
+		{ id: 'user', name: 'Usuario de sistema' }
+	]
+
+	const alice = {
+		id: uuid(),
+		name: 'Alice',
+		email: 'alice@example.com',
+		password: bcrypt.hashSync('1234567', 10),
+		role: "admin"
+	}
+	const dafne = {
+		id: uuid(),
+		name: 'Dafne',
+		email: 'dafne@example.com',
+		password: bcrypt.hashSync('1234567', 10),
+		role: "user"
+	}
+
+	await db.insert(Role).values(roles)
+	await db.insert(User).values([alice, dafne])
+
+	console.log("seed executed")
 }
