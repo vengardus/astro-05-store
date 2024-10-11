c517db9194e123e3b3c828ca51bdc7703f9e0eb0
Ismytv <ismytv@gmail.com>
2024-10-11 09:38:36 -0500

    feat(store):agrega tablas Product, ProductImage, imnagenes en /public/images

--- db/config.ts
+++ db/config.ts
@@ -18,10 +18,41 @@ const Role = defineTable({
   }
 })
 
+// STORE
+
+const Product = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    description: column.text(),
+    gender: column.text(),
+    price: column.number(),
+    sizes: column.text(),
+    slug: column.text(),
+    stock: column.number(),
+    tags: column.text(),
+    title: column.text(),
+    type: column.text(),
+
+    user: column.text({references: () => User.columns.id})
+  }
+})
+
+const ProductImage = defineTable({
+  columns: {      
+    id: column.text({ primaryKey: true }),
+    productId: column.text({references: () => Product.columns.id}),
+    image: column.text(),
+  }
+})
+
+
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
     User,
-    Role
+    Role,
+    Product,
+    Image,
   }
 });
