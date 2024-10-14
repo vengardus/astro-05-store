a4bc08f6eefc90832a13189bb82c09cf5925d01c
Ismytv <ismytv@gmail.com>
2024-10-13 19:22:54 -0500

    feat(store):graba item en cookies y las recupera en pages/cart/

---
+++ src/utils/cart-cookies-client.ts
@@ -0,0 +1,27 @@
+import type { CartItem } from "@/interfaces/cart/cart-item.interface";
+import Cookies from "js-cookie";
+
+export class CartCookiesClient {
+    static getCart():CartItem[] {
+        return  JSON.parse(Cookies.get('cart')?? '[]');
+    }
+
+    static addItem(cartItem:CartItem):CartItem[] {
+        const cart = CartCookiesClient.getCart();
+        const itemInCart = cart.find(item => item.productId == cartItem.productId && item.size == cartItem.size);
+        if(itemInCart) {
+            itemInCart.quantity += cartItem.quantity;
+        } else {
+            cart.push(cartItem);
+        }
+        Cookies.set('cart', JSON.stringify(cart));
+        return cart
+    }
+
+    static removeIten(productId:string, size:string):CartItem[] {
+        const cart = CartCookiesClient.getCart();
+        const updateCart = cart.filter(item => !(item.productId == productId && item.size == size));
+        Cookies.set('cart', JSON.stringify(updateCart));
+        return updateCart
+    }
+}
